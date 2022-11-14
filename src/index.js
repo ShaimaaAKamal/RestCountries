98507106f30987f11a57f7e57010b60b4b964450
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-14 16:54:51 +0200

    link bootstrap and fortawesome to our app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './Components/App/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
