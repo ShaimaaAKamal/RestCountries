8a15a7bca91a288dfa2d71eeef72d7756b5e9beb
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-15 13:50:48 +0200

    implement CountryCard Component Implementation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MasterLayout from '../MasterLayout/MasterLayout';
 import Home from '../Home/Home';
 import CountryDetails from '../CountryDetails/CountryDetails' 
+import CountryCard from '../CountryCard/CountryCard';
 import './App.css';
+
 
 function App() {
   let routes=createBrowserRouter([{
     path:'',element:<MasterLayout/>,children:[
       {index:true,element: <Home/>},
-      {path:'details',element: <CountryDetails/>}
+      {path:'details',element: <CountryDetails/>},
+      {path:'card',element: <CountryCard/>},
+
     ]
   }])
   return (
--- src/Components/CountryCard/CountryCard.jsx
+++ src/Components/CountryCard/CountryCard.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function CountryCard() {
   return (
-    <div>CountryCard</div>
+    <div className="card rounded-0" style={{width: '18rem'}}>
+    <img src="https://flagcdn.com/w320/pe.png" className="card-img-top rounded-0" alt="Flag Name"/>
+    <div className="card-body py-4 ps-4">
+      <h6 className="card-title mb-3">Country Name</h6>
+      <p className="card-text mb-1 info"><span>Population:</span><span >81,770,900</span></p>
+      <p className="card-text mb-1 info"><span>Region:</span><span >Europe</span></p>
+      <p className="card-text mb-2 info"><span>Capital:</span><span >Berlin</span></p>
+    </div>
+  </div>
   )
 }
