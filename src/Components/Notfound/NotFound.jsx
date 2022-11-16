ce97c80c21ce7d46776d56a88f132481eaed861b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-16 23:50:28 +0200

    change width

--- src/Components/Notfound/NotFound.jsx
+++ src/Components/Notfound/NotFound.jsx
@@ -4,7 +4,7 @@ import Image from '../../images/Notfound.jpg'
 export default function NotFound() {
   return (
     <div>
-      <img src={Image} alt="Not Found Image" className='w-100 vh-100' />
+      <img src={Image} alt="Not Found Image" className='w-100' />
     </div>
   )
 }
