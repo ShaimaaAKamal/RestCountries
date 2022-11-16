dc5f4701ce860e77e1c9ce8cc49b61691d1c653c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-16 19:33:56 +0200

    update index styling

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -4,12 +4,12 @@ export default function Filter(props) {
   return (
         <div className='container'>
                 <div className='d-flex flex-column flex-md-row justify-content-md-between align-items-md-center'>
-                    <div className="input-group input-group-lg search ">
+                    <div className="input-group input-group-lg search shadow-sm ">
                         <span className="input-group-text rounded-0" id="Search-Field"><i className="fa-solid fa-magnifying-glass"></i></span>
                         <input type="text" className="form-control rounded-0" aria-label="Search-Field" aria-describedby="Search-Field" placeholder='Search For a Country...' onKeyUp={(e)=>props.handleCountrySearch(e)}/>
                      </div>
 
-                    <select className="form-select form-select-lg filter rounded-0 " aria-label="RegionFilter"  onChange={ e => props.handleRegionSelect(e)}>
+                    <select className="form-select form-select-lg filter rounded-0 shadow-sm" aria-label="RegionFilter"  onChange={ e => props.handleRegionSelect(e)}>
                         <option value='filterByRegion'>Filter By Region</option>
                         {
                              Array.from(props.regions).map((region,index)=>  
