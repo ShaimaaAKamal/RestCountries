e34056a0246d3d7b12e343830b8834fa6babced2
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-16 14:33:27 +0200

    add Home index

--- src/Components/Header/CountryHeader.jsx
+++ src/Components/Header/CountryHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
+
 
 export default function CountryHeader() {
+  const navigate=useNavigate();
+
+  const GoHome=()=>{navigate('/')}
   return (
      <div className='py-3 header shadow-sm bgLight position-fixed w-100'>
         <div className="container textLight">
         <div className='d-flex justify-content-between align-items-center'>
-            <h5 className='mb-0'>Where in the world?</h5>
+            <h5 className='mb-0' onClick={GoHome}>Where in the world?</h5>
             <div>
               <div id='darkMode' className='d-flex align-items-center'>
               <i className="fa-regular fa-moon me-1"></i>
